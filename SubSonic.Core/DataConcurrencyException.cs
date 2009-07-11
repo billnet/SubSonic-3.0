dd166dd3b60e9a00b96b94698e64683df9315c3e
unknown <bif@.(none)>
2009-07-11 21:23:08 +0200

    Test

---
+++ SubSonic.Core/DataConcurrencyException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SubSonic
+{
+    /// <summary>
+    /// An exception that is thrown when a record cannot be updated because it was changed
+    /// by someone else
+    /// </summary>
+    public class DataConcurrencyException : Exception
+    {
+        public static string DefaultMessage = "The item you attempted to update has changed since it was last loaded. Your changes cannot be saved.";
+
+        public DataConcurrencyException() : base(DefaultMessage) { }
+        public DataConcurrencyException(string msg) : base(msg) { }
+    }
+}
---
+++ SubSonic.Core/RowVersionField.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SubSonic
+{
+    public class RowVersionField
+    {
+        public const string DbReservedFieldName = "__Version";
+
+        public RowVersionField()
+        {
+        }
+
+        public RowVersionField(Byte[] bytes)
+        {
+            _bytes = bytes;
+        }
+
+        public RowVersionField(string value)
+        {
+            _bytes = Convert.FromBase64String(value);
+        }
+
+        protected Byte[] _bytes = new Byte[] { };
+
+        public Byte[] Bytes
+        {
+            get
+            {
+                return _bytes;
+            }
+        }
+
+        public bool HasValue
+        {
+            get
+            {
+                return _bytes.Length > 0;
+            }
+        }
+
+        public void Update()
+        {
+            _bytes = Guid.NewGuid().ToByteArray();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is RowVersionField)
+                return ((RowVersionField)obj).ToString().Equals(this.ToString());
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return Convert.ToBase64String(_bytes);
+        }
+
+        public static RowVersionField FromString(string value)
+        {
+            return new RowVersionField(value);
+        }
+
+        public static RowVersionField FromBytes(Byte[] bytes)
+        {
+            return new RowVersionField(bytes);
+        }
+
+    }
+}
